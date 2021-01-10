5c65dfb4a679ecdb030a65e555d71f9b10d11209
JO <mnj190@naver.com>
2021-01-10 09:14:10 +0900

    commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-    res.send(`Response Complate`);
+app.get('/api/host', (req, res) => {
+    res.send({ host : 'sejun' });
 })
 
 app.listen(PORT, () => {
     console.log(`Server On : http://localhost:${PORT}/`);
-  })
+})
